6586d7b05595f88ecca725cedfc883433c81f22e
Gokula Krishnan.K <65504920+krishnansai@users.noreply.github.com>
2025-07-04 07:07:15 +0530

    Update db.js

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,10 +1,10 @@
-const {createPool} = require('mysql2/promise');
+const { createPool } = require('mysql2/promise');
 
 const pool = createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'todolist',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'todolist',
   namedPlaceholders: true,
 });
 
